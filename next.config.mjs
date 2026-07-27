49ffa80e455e310875feead4a53f1343b18b3291
Akshat Thakur <146325203+Akshatthakur22@users.noreply.github.com>
2026-07-27 12:36:47 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -15,11 +15,11 @@ const securityHeaders = [
     key: "Content-Security-Policy",
     value: [
       "default-src 'self'",
-      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://checkout.razorpay.com https://va.vercel-scripts.com",
+      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://checkout.razorpay.com https://va.vercel-scripts.com https://www.googletagmanager.com https://www.google-analytics.com",
       "style-src 'self' 'unsafe-inline'",
       "img-src 'self' data: blob: https:",
       "font-src 'self' data:",
-      "connect-src 'self' https://api.razorpay.com https://checkout.razorpay.com https://vitals.vercel-insights.com",
+      "connect-src 'self' https://api.razorpay.com https://checkout.razorpay.com https://vitals.vercel-insights.com https://www.google-analytics.com https://www.googletagmanager.com",
       "frame-src https://api.razorpay.com https://checkout.razorpay.com https://www.google.com https://maps.google.com",
       "object-src 'none'",
       "base-uri 'self'",
